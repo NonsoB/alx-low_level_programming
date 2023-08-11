496bf969ee8ac2214c645637df7bc899629daeb9
NonsoB <okorojichinonso1@gmail.com>
2023-08-10 19:53:12 -0700

    it works

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * main- This program prints type
+ * main - This program prints type
  * Return: 0
  */
 int main(void)
-printf("Size of char: %lu byte(s)\n", sizeof(char));
-printf("Size of int: %lu byte(s)\n", sizeof(int));
-printf("Size of long int: %lu byte(s)\n", sizeof(long));
-printf("Size of long long int: %lu byte(s)\n", sizeof(long long));
-printf("Size of float: %lu byte(s)\n", sizeof(float));
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int))
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
