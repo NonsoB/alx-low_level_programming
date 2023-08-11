fd79b199c7f47105f64fecad4b890d7c76513fa4
NonsoB <okorojichinonso1@gmail.com>
2023-08-10 19:15:12 -0700

    Lets hope it works

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,10 @@
  * Return: 0
  */
 int main(void)
-{
-printf("Size a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 4 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)");
+printf("Size of char: %lu byte(s)\n", sizeof(char));
+printf("Size of int: %lu byte(s)\n", sizeof(int));
+printf("Size of long int: %lu byte(s)\n", sizeof(long));
+printf("Size of long long int: %lu byte(s)\n", sizeof(long long));
+printf("Size of float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
