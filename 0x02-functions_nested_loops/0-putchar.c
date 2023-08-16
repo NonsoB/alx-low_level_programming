8ab3a49ca91e053dfd91c83f8b56d314d9a9b90c
NonsoB <okorojichinonso1@gmail.com>
2023-08-16 08:37:46 -0700

    i have tried

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,29 +1,33 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * main- This program is supposed to print putchar and a
- * new line. I'm just gonna attempt it to see how much I've
- * learnt so far. fingers crossed.
- * c = character
- * _putchar = prints out the character to standard output
- * Return : 1
+ * _putchar - Custom implementation of the putchar function.
+ * @c: The character to be printed.
+ *This function writes a single character to the standard output.
+ * Returns:
+ * On success, the number of bytes written (1).
+ * On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * main - Entry point of the program.
+ * This function prints the word "_putchar" followed by a newline.
+ * Return: 0 (success)
  */
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
-
-return (0);
-}
-
-int _putchar(char c)
-{
-    return write(1, &c, 1);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
