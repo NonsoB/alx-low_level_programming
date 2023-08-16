a0ee17eab03ad7ec81fa2098d4076fde78c1cf78
NonsoB <okorojichinonso1@gmail.com>
2023-08-16 06:28:15 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- This program is supposed to print putchar and a
+ * new line. I'm just gonna attempt it to see how much I've
+ * learnt so far. finers crossed.
+ * Return: 0 (success)
+ */
+char main(void)
+{
+	printf("\_putchar\n");
+	return (0);
+}
