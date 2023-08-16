072d0df2230a329321882fea354cff2d8e8d2153
NonsoB <okorojichinonso1@gmail.com>
2023-08-16 14:41:52 -0700

    it works

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet- a function that prints the alphabets
+ * "a" - "z" in small letters
+ * Return: 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+	return (0);
+}
+
