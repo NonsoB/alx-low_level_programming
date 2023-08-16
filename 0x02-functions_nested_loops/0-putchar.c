d96a0007e0bfce0d15a5942f31a10900c367d3e4
NonsoB <okorojichinonso1@gmail.com>
2023-08-16 07:12:12 -0700

    i hope

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include "0-putchar.c"
 /**
  * main- This program is supposed to print putchar and a
  * new line. I'm just gonna attempt it to see how much I've
