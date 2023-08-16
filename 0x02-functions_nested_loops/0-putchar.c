495556f481a522abe3b7380985b16e88fd7d2f64
NonsoB <okorojichinonso1@gmail.com>
2023-08-16 07:46:40 -0700

    how

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,24 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main- This program is supposed to print putchar and a
  * new line. I'm just gonna attempt it to see how much I've
- * learnt so far. finers crossed.
- * Return: 1 (success)
+ * learnt so far. fingers crossed.
+ * c = character
+ * _putchar = prints out the character to standard output
+ * Return : 1
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
