3c3c3931c852f8e71c806a3c0a38fa65c9f19fc2
NonsoB <okorojichinonso1@gmail.com>
2023-08-16 07:21:41 -0700

    i hope

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,12 +4,9 @@
  * main- This program is supposed to print putchar and a
  * new line. I'm just gonna attempt it to see how much I've
  * learnt so far. finers crossed.
- * @c: character
  * Return: 1 (success)
  */
-int _putchar(char c);
-
-int main(void)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
