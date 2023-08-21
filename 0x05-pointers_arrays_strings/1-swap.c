1ebc5c183da94b43a296cbb0cf7450c80256f823
NonsoB <okorojichinonso1@gmail.com>
2023-08-21 15:19:33 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,22 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 
-void swap_int(int *, int *)
-
 /**
- * main - check the code
  * swap_int - a function that swaps variables
+ * @a: a variable name
+ * @b: a vsriable name
  * Return: Always 0.
  */
-int main(void)
+void swap_int(int *, int *)
 {
 	int a;
 	int b;
 
 	a = 98;
 	b = 42;
-	printf("a=%d, b=%d\n", a, b);
-	swap_int(&a, &b);
-	printf("a=%d, b=%d\n", a, b);
-	return (0);
+	*a = b;
+	*b = a;
 }
